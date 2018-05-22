ffaadc0431c8bfed0799d5fca0900b73bc92ee57
Ye Thu Aung <yethuaung481995@gmail.com>
2018-05-22 19:09:23 +0630

    add sign in

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/signup', function(req, res, next) {
+  res.render('commons/signup', { title: 'Express' });
+}); 
 
 module.exports = router;
