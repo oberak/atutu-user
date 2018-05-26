b9df091b792e3775274a24442cbf4bec2db36819
Ye Thu Aung <yethuaung481995@gmail.com>
2018-05-26 18:11:10 +0630

    add TODO

--- routes/campaign.js
+++ routes/campaign.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+//TODO must finish page
 router.get('/add', function(req, res, next) {
   res.render('campaign/campaign-add', { title: 'Express' });
 });
