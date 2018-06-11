ed6d51a3b27040275caf41ce4379f08e0ec8aaa4
Aye-Thet <ayethet107@gmail.com>
2018-06-11 16:21:12 +0630

    campaign view

--- routes/campaign.js
+++ routes/campaign.js
@@ -2,7 +2,15 @@ var express = require('express');
 var router = express.Router();
 //TODO must finish page
 router.get('/add', function(req, res, next) {
-  res.render('campaign/campaign-add', { title: 'Express' });
+  res.render('campaign/campaign-add');
+});
+
+router.get('/view', function(req, res, next) {
+  res.render('campaign/campaign-view');
+});
+
+router.get('/list', function(req, res, next) {
+  res.render('campaign/campaign-list');
 });
 
 module.exports = router;
